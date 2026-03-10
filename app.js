8a0ebb68bf5159c0e69d30eb0f86fa902e1597bc
name <email@mail>
2026-03-10 16:06:10 +0100

    structure de l'app

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const signinRouter = require('./routes/signin');
 
 const app = express();
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/signin', signinRouter);
 
 app.listen(3000);
--- routes/signin.js
+++ routes/signin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('<h1>Hello in my app</h1>');
+  res.send('<h1>Signin</h1>');
 });
 
 module.exports = router;
