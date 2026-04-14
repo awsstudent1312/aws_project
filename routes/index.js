93dcdf896404a0769369f9bf073935cce5280dcd
student1312 <email@mail>
2026-04-14 22:07:23 +0200

    ajout d'une configuration pour vercel

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // res.render('client.html');
-  res.sendFile(__dirname + "/views/client.html");
+  res.render("client.html");
+  // res.sendFile(__dirname + "/views/client.html");
 });
 
 module.exports = router;
